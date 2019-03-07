c0b8508050e692891f73dd98617a5cf9af5f6bbb
tcheymol <thibaut.cheymol@gmail.com>
2019-03-07 14:06:29 +0100

    🛣 Make login button redirect to login page

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 import GifsList from './components/GifsList';
@@ -58,7 +59,7 @@ class HomePage extends Component {
                             Truckfinder
                         </div>
                         <div className="header-buttons">
-                            <button>Log in</button>
+                            <Link to="/login">Log in</Link>
                         </div>
                     </div>
                 </header>
