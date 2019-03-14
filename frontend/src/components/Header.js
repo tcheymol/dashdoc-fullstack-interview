219879527745e7510d284fdfbfa2a33565762d31
tcheymol <thibaut.cheymol@gmail.com>
2019-03-14 18:54:33 +0100

    🐸 Display username in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,35 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-const fetchCurrentUser = async () => {
-    const token = localStorage.getItem('token');
-     fetch('http://127.0.0.1:8000/me/', {
-            method: 'GET',
-            headers: {
-                "Authorization": `Token ${token}`,
-            },
-        })
-        .then((response) => {
-            if(response.status!==200) {
-              throw new Error(response.status)
-            }
-            return response.json()
-        })
-        .then((json) => {
-            console.log(json)
-            localStorage.setItem('user', JSON.stringify(json));
-        })
-        .catch((error) => {
-            alert('There has been an error fetching the use, please refresh');
-        });
-}
 
 class Header extends React.Component{
+    state = {
+        username: null,
+    };
+
+    fetchCurrentUser = async () => {
+         const token = localStorage.getItem('token');
+         fetch('http://127.0.0.1:8000/me/', {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Token ${token}`,
+                },
+            })
+            .then((response) => {
+                if(response.status!==200) {
+                  throw new Error(response.status)
+                }
+                return response.json()
+            })
+            .then((json) => {
+                localStorage.setItem('user', JSON.stringify(json));
+                this.setState({ username: json.username });
+                return json;
+            })
+            .catch((error) => {
+                alert('There has been an error fetching the use, please refresh');
+            });
+    };
+
     componentDidMount() {
-        fetchCurrentUser();
+        this.fetchCurrentUser();
     }
 
     render() {
+        const { username } = this.state;
         return(
             <header className="header">
                 <div className="header-inner">
@@ -40,9 +47,12 @@ class Header extends React.Component{
                         </span>
                         Truckfinder
                     </div>
-                    <div className="header-buttons">
-                        <Link to="/login">Log in</Link>
-                    </div>
+                    { username !== null
+                        ? <div>{username}</div>
+                        : <div className="header-buttons">
+                            <Link to="/login">Log in</Link>
+                        </div>
+                    }
                 </div>
             </header>
         )
