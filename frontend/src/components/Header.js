3a66c4db21bd885f04b374c00066844e87c8b391
tcheymol <thibaut.cheymol@gmail.com>
2019-03-14 18:49:45 +0100

    👨‍💻 Add user fetch on header mount

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
+const fetchCurrentUser = async () => {
+    const token = localStorage.getItem('token');
+     fetch('http://127.0.0.1:8000/me/', {
+            method: 'GET',
+            headers: {
+                "Authorization": `Token ${token}`,
+            },
+        })
+        .then((response) => {
+            if(response.status!==200) {
+              throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then((json) => {
+            console.log(json)
+            localStorage.setItem('user', JSON.stringify(json));
+        })
+        .catch((error) => {
+            alert('There has been an error fetching the use, please refresh');
+        });
+}
 
 class Header extends React.Component{
+    componentDidMount() {
+        fetchCurrentUser();
+    }
+
     render() {
         return(
             <header className="header">
