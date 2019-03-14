d3fd065d7343f9fc96cd33fa7da31712f4b4443b
tcheymol <thibaut.cheymol@gmail.com>
2019-03-14 08:55:12 +0100

    ✨ Create Login page component

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./components/Header";
+import "./App.css";
+
+const LoginPage = () => (
+    <React.Fragment >
+        <Header/>
+        <div className="app">
+            <h1>Login</h1>
+        </div>
+    </React.Fragment>
+);
+
+export default LoginPage;
+
--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
 
 function AppRouter() {
   return (
     <Router>
         <div>
             <Route path="/" exact component={HomePage} />
-            <Route path="/login/" component={() => <div />} />
+            <Route path="/login/" component={() => <LoginPage />} />
         </div>
     </Router>
   );
