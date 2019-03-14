4d8ca1f1bca1a346563bb526be6d73f9454bf97b
tcheymol <thibaut.cheymol@gmail.com>
2019-03-14 18:45:09 +0100

    🏆 Make header class component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,20 +3,24 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 
-const Header = () => (
-    <header className="header">
-        <div className="header-inner">
-            <div className="header-title">
-                <span role="img" aria-label="truck-logo">
-                    🚚
-                </span>
-                Truckfinder
-            </div>
-            <div className="header-buttons">
-                <Link to="/login">Log in</Link>
-            </div>
-        </div>
-    </header>
-);
+class Header extends React.Component{
+    render() {
+        return(
+            <header className="header">
+                <div className="header-inner">
+                    <div className="header-title">
+                        <span role="img" aria-label="truck-logo">
+                            🚚
+                        </span>
+                        Truckfinder
+                    </div>
+                    <div className="header-buttons">
+                        <Link to="/login">Log in</Link>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
 
 export default Header;
