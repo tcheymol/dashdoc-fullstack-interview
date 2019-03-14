a286369b8bde7b172ebfa45fe1d73b35be839cc3
tcheymol <thibaut.cheymol@gmail.com>
2019-03-14 09:09:26 +0100

    ✨ Add login form

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -7,7 +7,12 @@ const LoginPage = () => (
     <React.Fragment >
         <Header/>
         <div className="app">
-            <h1>Login</h1>
+            <h1>Please Login</h1>
+            <form>
+                <input type="text" name="username"/>
+                <input type="password" name="password"/>
+                <button type="submit">Login</button>
+            </form>
         </div>
     </React.Fragment>
 );
